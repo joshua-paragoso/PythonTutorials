7befb652f2dd94def3c15cad1d093323b3743c32
joshypuu <jparagos@asu.edu>
2020-07-01 12:55:21 -1000

    First commit

---
+++ 1_HelloWorld.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+#this prints out 'hello world'
+print("This line will be printed.")
---
+++ 3_List.py
@@ -0,0 +1,58 @@
+#L ists are very similar to arrays. 
+# They can contain any type of variable, and they can 
+# contain as many variables as you wish. Lists can also 
+# be iterated over in a very simple manner. 
+# Here is an example of how to build a list.
+
+#empty list
+mylist = []
+
+#adds contents to list
+mylist.append(1) 
+mylist.append(2)
+mylist.append(3)
+
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
+
+mylist = [1,2,3]
+print(mylist[0])
+
+#excericse:
+print("-------Excercise---------")
+# In this exercise, you will need to 
+# add numbers and strings to the correct 
+# lists using the "append" list method. 
+# You must add the numbers 1,2, and 3 to the "numbers" list, 
+# and the words 'hello' and 'world' to the strings variable.
+
+# You will also have to fill in the variable 
+# second_name with the second name in the names 
+# list, using the brackets operator []. 
+# Note that the index is zero-based, 
+# so if you want to access the second item in the list, 
+# its index will be 1.
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("John");
+strings.append("Eric");
+strings.append("Jessica");
+# write your code here
+second_name = names[1];
+
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
